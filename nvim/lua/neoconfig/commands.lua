2d9079c5cb9bbf6c4e505d78a9580f18a2d471e6
Debdut Chakraborty <debdut.chakraborty@rocket.chat>
2023-06-30 17:27:19 +0530

    + helper command for lsp root directory

--- nvim/lua/neoconfig/commands.lua
+++ nvim/lua/neoconfig/commands.lua
@@ -1,5 +1,7 @@
 vim.api.nvim_create_user_command("LoadLocalPlugin", function(opts)
-	if #opts.fargs == 0 then return end
+	if #opts.fargs == 0 then
+		return
+	end
 	local plugin = opts.fargs[0]
 	package.loaded[plugin] = nil
 	local ok, ret = pcall(require, plugin)
@@ -12,3 +14,26 @@ vim.api.nvim_create_user_command("LoadLocalPlugin", function(opts)
 	end
 end, { nargs = 1 })
 
+local lspconfig_configs = require("lspconfig.configs")
+local lspconfig_util = require("lspconfig.util")
+local plenary_filetype = require("plenary.filetype")
+local lsp_servers_configured = require("neoconfig.lsp.servers")
+
+vim.api.nvim_create_user_command("LspRootDir", function(opts)
+	local file = vim.api.nvim_buf_get_name(0)
+	local get_configured_server = function()
+		for _, client in pairs(lspconfig_util.get_other_matching_providers(plenary_filetype.detect(file))) do
+			for _, server in pairs(lsp_servers_configured) do
+				if server == client.name then
+					return server
+				end
+			end
+		end
+	end
+	local config = lspconfig_configs[get_configured_server()]
+	if not config then
+		return
+	end
+	local root_dir = config.get_root_dir(lspconfig_util.path.dirname(file))
+	vim.notify(root_dir)
+end, { nargs = 0 })
