77d36cff5a8c04e59f6fc17da4e43a9e8fe44c3f
Debdut Chakraborty <debdutdeb@outlook.com>
2023-10-10 01:11:08 +0530

    tools: dlv script to start remote sessions with tmux

---
+++ _bin/go-debug.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -m
+
+_args=(exec --headless --continue --accept-multiclient --listen "127.0.0.1:${DLV_port:=33000}")
+
+if (($# == 0)) || [[ -d "$1" ]]; then
+	go build -gcflags="all=-N -l" "${1:-.}"
+	_args+=("$(basename "$(go mod why | tail -n1)")")
+else
+	_args+=("$1")
+	shift
+	if (($# > 0)); then _args+=("$@"); fi
+fi
+
+dlv "${_args[@]}" &
+sleep "${DLV_sleep_delay:-3}"
+tmux split-window -h "dlv connect 127.0.0.1:$DLV_port"
+fg
