35fa8b70c5e28809e694894e19d3162d95dc4000
Debdut Chakraborty <debdutdeb@outlook.com>
2023-12-22 00:53:50 +0530

    neovim: fix qflist bindings

--- nvim/after/ftplugin/qf.lua
+++ nvim/after/ftplugin/qf.lua
@@ -1,15 +1,2 @@
-local remap = require('debdut.keymap')
-remap.nnoremap("o", function()
-	-- FIXME i don't know why feed_keys does not trigger the next keymap
-	-- initially pressing 'o' closed the split but then it didn't once
-	-- i changed to feed_keys
-	-- Likelyhood of this breaking is high
-	remap.nsend_keys "<CR><C-w>p"
-end)
-remap.nnoremap("<cr>", function()
-	local w = vim.api.nvim_get_current_win()
-	vim.cmd "normal! <CR>"
-	if vim.api.nvim_win_is_valid(w) then vim.api.nvim_win_close(w, true) end
-end)
-
-vim.b.debdut='qf'
+vim.api.nvim_buf_set_keymap(0, "n", "o", "<cr><C-w>p", {silent = true})
+-- vim.api.nvim_buf_set_keymap(0, "n", "<cr>", '<CR>:wincmd p | quit<cr>', {silent = true})
