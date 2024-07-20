5c1045010ae7f0db27c6b7d7f3cc9dcd1ee07a87
Debdut Chakraborty <debdutdeb@outlook.com>
2024-07-20 14:50:22 +0530

    neovim: add colorscheme palenightfall

--- nvim/lua/my/plugins/colorschemes.lua
+++ nvim/lua/my/plugins/colorschemes.lua
@@ -1,3 +1,4 @@
 return {
-	{ 'savq/melange-nvim', lazy = false, priority = 999, config = function() vim.cmd 'colorscheme melange' end },
+	{ 'savq/melange-nvim', enabled = false, lazy = false, priority = 999, config = function() vim.cmd 'colorscheme melange' end },
+	{ 'JoosepAlviste/palenightfall.nvim', lazy = false, priority = 1000, config = function() vim.cmd 'colorscheme palenightfall' end, opts = { transparent = true } },
 }
