47c177aef1bbf983f4a87c886068581b4847ae2b
Debdut <debdutdeb@outlook.com>
2026-01-27 01:36:33 +0530

    tmux: add logging plugin to keep tmux history

--- tmux_setup.sh
+++ tmux_setup.sh
@@ -3,3 +3,9 @@
 # boilerplate putting here to remember later
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+if [ "$(uname)" = "Darwin" ]; then
+	if ! command -v brew 2>&1 >/dev/null; then echo "[Error] homebrew must be installed" >&2 && exit 1; fi
+
+	brew install ansifilter
+fi
